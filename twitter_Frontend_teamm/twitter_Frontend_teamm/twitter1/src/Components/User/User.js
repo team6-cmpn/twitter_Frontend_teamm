9a387c842f8abaa90aa250494395196aa2d17c0a
MennaHesham1128 <monmonh2000@gmail.com>
2022-05-10 22:02:52 +0200

    Follow button

--- twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/User/User.js
+++ twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/User/User.js
@@ -1,5 +1,6 @@
 import '../Profile/Profile.css'
 import '../Notifications.css'
+import './User.css'
 import React from 'react';
 import  { useState} from 'react';
 // import placeholder from 'react-image-placeholder';
@@ -8,25 +9,35 @@ import { Link } from 'react-router-dom';
 import  getUsernames    from '../Profile/ProfileMock';
 import Trends from "../Widgets/Trends";
 import Sidebar from "../Sidebar/Sidebar";
-import {GrLocation} from "react-icons/gr"
-import {BiLink, BiArrowBack} from "react-icons/bi"
-import {getUserInfo} from '../Profile/backEndProfile'
-// import { GetPostTweet } from "../homepage/feedmock";
-// import Post from "../homepage/Post";
+import {GrLocation} from "react-icons/gr";
+import {BiLink, BiArrowBack} from "react-icons/bi";
+import {getUserInfo} from '../Profile/backEndProfile';
+import {Modal} from "antd";
+import "../Widgets/FriendSuggestions/FriendSuggestionItem/FriendSuggestionItem.css";
+import { GetPostTweet } from "../homepage/feedmock";
+import Post from "../homepage/Post";
+import { RecoilRoot } from "recoil";
+import { Menu } from 'antd';
+import { Popover } from 'antd';
 
 function User(){
     const [isTab, setIsTab] = useState(1);
-    // const [twetted, postedtweet] = React.useState([]);
+    const [twetted, postedtweet] = React.useState([]);
     
-    // React.useEffect(() => {
-    //   (async () => {
-    //     const resp = await GetPostTweet();
-    //     postedtweet(resp);
-    //   })();
-    // }, []);
+    
+    React.useEffect(() => {
+      (async () => {
+        const resp = await GetPostTweet();
+        postedtweet(resp);
+      })();
+    }, []);
     // const [{alt, src}, setImg] = useState({
     //     src: placeholder,
     // });
+    const [isPopover, setPopover] = useState(true);
+   
+    
+    
 
     
     const [Username, setUsernames] = React.useState([]);
@@ -67,9 +78,26 @@ function User(){
     //     website: editwebsite,
     //     img: {alt,src},
     // }
+    const [textState, setTextState] = useState("Follow");
+    const toggleText = () => {
+      setTextState((state) => (state === "Following" ? "Follow" : "Following"));
+    };
+    const [isModalVisible, setModalVisible] = useState(false);
+    const onSubModel = (stateMain = true) => {
+      setModalVisible(stateMain);
+    };
+    const onExist = () => {
+      setModalVisible(false);
+    };
+
     const user=getUserInfo();
     user.then(data=>{setItem(data)});
     console.log(Item);
+    const content = (
+        <div>
+          Menna
+        </div>
+      );
     return(
     <div>
     <Sidebar />
@@ -107,7 +135,41 @@ function User(){
 
                 <br></br>
                 <div>
-                <button id="FollowButton" class="ButtonEditProfile" ><span>Follow</span></button>
+                    <Popover
+                    content={content}
+                    title="Title"
+                    trigger="click"
+                    visible={isPopover}
+                    >
+                        <button id="More" class="More" ><span>...</span></button>
+                    </Popover>
+                    <button id="FollowButton" class="ButtonFollow" onClick={() => {
+                        if (textState==="Follow")
+                        toggleText();
+                        else
+                        onSubModel();}}>
+                        {textState}   
+                    </button> 
+                    
+                
+                <Modal
+                    style={{textAlign: "center"}}
+                
+                    visible={isModalVisible}
+                    bodyStyle={{height: 300, font: "Helvetica", textAlign: "left"}}
+                    width={500}
+                    alignItems={{top: Window}}
+                    onCancel={() => setModalVisible(false)}
+                    footer={null}
+                    
+                >
+                    <div className="for_model">
+                    <div style={{fontSize: "200%", marginTop: "10px", color: "black", textAlign: "center" }}>Unfollow this user?</div>
+                    <div style={{padding: "30px 30px"}}>Their Tweets will no longer show up in your home timeline. You can still view their profile, unless their Tweets are protected. </div>
+                    <button id="Unfollow" onClick={()=>{toggleText(); onExist();}} className="followButton">Unfollow</button>
+                    </div>
+                </Modal>
+                
                 </div>
                 <br></br>
                 <div id="followers"className="FollowLink">
@@ -119,6 +181,8 @@ function User(){
                 </div>
                 <br></br>
                 <br></br>
+            <div>
+             <RecoilRoot>
                 <div className="notificationsCategory">
                     <div id="tweets"
                     className={isTab === 1 && "notificationActive"}
@@ -144,7 +208,10 @@ function User(){
                     >
                     <span>Likes</span>
                     </div>
-                    {/* <article>
+                    </div>
+                    <article>
+                        {
+                         (isTab===1)?
                         
                             <>
 
@@ -159,11 +226,57 @@ function User(){
                                  date={userlist.date}
                                  />))}
                             </>
+                         :
+                         (isTab===2)?
+                            <>
+
+                            {twetted.map((userlist, index) => (
+                                 <Post
+                                 key={index}
+                                 displayName={userlist.displayName}
+                                 username={userlist.username}
+                                 text={userlist.text}
+                                 image={userlist.image}
+                                 avatar={userlist.avatar}
+                                 date={userlist.date}
+                                 />))}
+                            </>
+                        : (isTab===3)?
+                            <>
+
+                            {twetted.map((userlist, index) => (
+                                 <Post
+                                 key={index}
+                                 displayName={userlist.displayName}
+                                 username={userlist.username}
+                                 text={userlist.text}
+                                 image={userlist.image}
+                                 avatar={userlist.avatar}
+                                 date={userlist.date}
+                                 />))}
+                            </>
+                        : 
+                            <>
+                            {twetted.map((userlist, index) => (
+                                 <Post
+                                 key={index}
+                                 displayName={userlist.displayName}
+                                 username={userlist.username}
+                                 text={userlist.text}
+                                 image={userlist.image}
+                                 avatar={userlist.avatar}
+                                 date={userlist.date}
+                                 />))}
+                            </> 
                         
-            
+                        
+                        }
           
            
-                    </article> */}
+                    </article>
+                
+                </RecoilRoot>
+                
             </div>
                 
         

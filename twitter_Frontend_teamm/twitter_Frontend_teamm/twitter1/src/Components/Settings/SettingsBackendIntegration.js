5d7cbc1a18d813a8e8b6b68190efbbe77d4baa64
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2022-05-16 09:30:41 +0200

    change(phone,email,username) integrated with BE FINALLY

--- twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Settings/SettingsBackendIntegration.js
+++ twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Settings/SettingsBackendIntegration.js
@@ -41,6 +41,111 @@ export async function Post_ChangePassword(){
 };
 
 
+export async function Post_ChangeEmail(){
+  var message;
+  const body =  { email:sessionStorage.getItem('email')
+      }
+    console.log(`${localStorage.getItem('token')}`)
+    await axios
+      .post(`${Configure.backURL}user/changeemail/`,body, {
+        
+
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'x-access-token': `${localStorage.getItem('token')}`,
+        },
+       
+        
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          message='';
+          
+        }
+        else if (response.status === 403) {
+          message='  ';
+          console.log(response.data.message);
+        }
+        else if(response.status === 401 || response.status === 404 || response.status === 500 ){
+          message=response.data.message;
+        }
+
+      }).catch(error => {
+          console.log(error);
+          });
+    return message;
+};
+export async function Post_ChangeUsername(){
+  var message;
+  const body =  { username:sessionStorage.getItem('username')
+      }
+    console.log(`${localStorage.getItem('token')}`)
+    await axios
+      .post(`${Configure.backURL}user/changeusername/`,body, {
+        
+
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'x-access-token': `${localStorage.getItem('token')}`,
+        },
+       
+        
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          message='';
+          
+        }
+        else if (response.status === 403) {
+          message='  ';
+          console.log(response.data.message);
+        }
+        else if(response.status === 401 || response.status === 404 || response.status === 500 ){
+          message=response.data.message;
+        }
+
+      }).catch(error => {
+          console.log(error);
+          });
+    return message;
+};
+export async function Post_ChangePhone(){
+  var message;
+  const body =  { phone_number:sessionStorage.getItem('phone_number')
+      }
+    console.log(`${localStorage.getItem('token')}`)
+    await axios
+      .post(`${Configure.backURL}user/changePhoneNumber/`,body, {
+        
+
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'x-access-token': `${localStorage.getItem('token')}`,
+        },
+       
+        
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          message='';
+          
+        }
+        else if (response.status === 403) {
+          message='  ';
+          console.log(response.data.message);
+        }
+        else if(response.status === 401 || response.status === 404 || response.status === 500 ){
+          message=response.data.message;
+        }
+
+      }).catch(error => {
+          console.log(error);
+          });
+    return message;
+};
 export async function Put_DeactivateAccount(){
     var message;
     const body={};

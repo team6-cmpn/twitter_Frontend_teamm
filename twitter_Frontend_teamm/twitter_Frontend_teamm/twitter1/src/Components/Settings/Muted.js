0c7f18ac8e284565cd761136ffa93daea58e4203
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2022-05-20 01:59:41 +0200

    entered selected profile

--- twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Settings/Muted.js
+++ twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Settings/Muted.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./settingsSubmenus.css";
 import {Avatar} from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Muted = ({ MutedAccount }) => {
   return (
     <div className={`SubsettingsCont `}>
        <Avatar classname="Avatar"src={''} />
       <div className="text_discrip">
-     
 
-          <h5>{MutedAccount.name}</h5>
+        <h5 onClick={localStorage.setItem("clicked_userID",MutedAccount._id)}>
+        <Link to={`/${MutedAccount.username}`}>{MutedAccount.name}
+        </Link></h5>
           <h6>{MutedAccount.username}</h6>
       </div>
           

a8444ed7d3b27278bd48f32a37951fdc606493ff
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2022-05-22 04:22:00 +0200

    added condtions

--- twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Notifications.js
+++ twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Notifications.js
@@ -8,27 +8,27 @@ import SettingsBox from "./SettingsBox/SettingsBox";
 import LikedYou from "./LikedYou";
 import getFollowingUsers from "./NotificationsMock";
 import { getfollowedUsers } from "./NotificationsMock";
-import {getLikedUsers} from "./NotificationsMock";
+import { getLikedUsers } from "./NotificationsMock";
 import { GetNotificationsFavourites } from "./NotificationBackend";
 import { GetNotifications } from "./NotificationBackend";
 import LikedYouBe from "./LikedYouBe";
 import Notified from "./Notified";
 
 /**
- * [the notification component is mainly used to get the notifications from the server to the user to see it 
+ * [the notification component is mainly used to get the notifications from the server to the user to see it
  *  and it contains all the mentions and liked posts and followers you got and i made a menu to scoope on the mentioned posts only
  * no need to scroll down to see the mentioned post you need to see
  * ]
  * @param [it doesnt take any parameters]
  * @returns[it doesnt return any thing ]
- * 
+ *
  */
 function Notifications() {
   const [Blockeddays, setBlockeddays] = React.useState([]);
   const [LikedUser, setLikedUser] = React.useState([]);
   const [followedyou, setfollowedyou] = React.useState([]);
-  const [BE,setNotifications]=React.useState([]);
-  const [notifi,setNotificationsFavs]=React.useState([]);
+  const [BE, setNotifications] = React.useState([]);
+  const [notifi, setNotificationsFavs] = React.useState([]);
   const [isAll, setIsAll] = React.useState(true);
   document.title = "Notifications / Twitter";
   React.useEffect(() => {
@@ -45,18 +45,19 @@ function Notifications() {
       setLikedUser(resp);
     })();
   }, []);
-  React.useEffect(()=>{ 
-     (async () => {
-    const resp = await GetNotifications();
-    setNotifications(resp);
-     })();
-  },[])
-  React.useEffect(()=>{ 
+  React.useEffect(() => {
     (async () => {
-   const resp = await GetNotificationsFavourites();
-   setNotificationsFavs(resp);
+      const resp = await GetNotifications();
+      setNotifications(resp);
     })();
-    },[])
+  }, []);
+  React.useEffect(() => {
+    (async () => {
+      const resp = await GetNotificationsFavourites();
+      setNotificationsFavs(resp);
+    })();
+  }, []);
+  
   return (
     <SettingsBox>
       <div className="Notimenu" id="NotificationMenu">
@@ -82,9 +83,12 @@ function Notifications() {
         <article>
           {isAll ? (
             <>
-
               {BE.map((Notifications, index) => (
-              <Notified key={index} notify={Notifications} nType={Notifications} />
+                <Notified
+                  key={index}
+                  notify={Notifications}
+                  nType={Notifications}
+                />
               ))}
 
               {/* {Blockeddays.map((Blockeddays,index)=>(
@@ -100,8 +104,6 @@ function Notifications() {
               {followedyou.map((userNotification, index) => (
               <FollowedYou key={index} followingUser={userNotification} />
               ))} */}
-            
-              
             </>
           ) : (
             <>
@@ -109,7 +111,7 @@ function Notifications() {
               <LikedYou key={index} likePost={userNotification} />
               ))} */}
               {notifi.map((favourites, index) => (
-              <LikedYouBe key={index} liked={favourites} />
+                <LikedYouBe key={index} liked={favourites} />
               ))}
             </>
           )}
--- twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Notified.js
+++ twitter_Frontend_teamm/twitter_Frontend_teamm/twitter1/src/Components/Notified.js
@@ -2,52 +2,60 @@ import React from "react";
 import "./LikedYou.css";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import { Avatar } from "@material-ui/core";
-import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
-const Notified = ({ notify,nType }) => {
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+const Notified = ({ notify, nType }) => {
   return (
     <div className="likedYou" id="likedyou">
       <NotificationsActiveIcon />
-      
+      {notify.notificationContent.text ? (
+        <>
+          {nType.notificationType === "block" && (
+            <div>
+              <span>
+                <b>{notify.notificationHeader.text}</b>
+              </span>
+            </div>
+          )}
+          {nType.notificationType === "unblock" && (
+            <div>
+              <span>
+                <b>{notify.notificationHeader.text}</b>
+              </span>
+            </div>
+          )}
 
-        {nType.notificationType==='block'&&
-
-        <div>
-
-        <span>
-        <b>{notify.notificationHeader.text}</b>
-        </span>
-        </div>}
-        {nType.notificationType==='unblock'&&
-
-        <div>
-
-        <span>
-        <b>{notify.notificationHeader.text}</b>
-        </span>
-        </div>}
-
-        { nType.notificationType==='favourite'&&
-        <div>
-         <div>
-            <Avatar src={notify.notificationHeader.images} />
-          
-        </div>
-        <span>
-          <b>{notify.notificationHeader.text}</b> 
-        </span>
-        <span>{notify.notificationContent.text}</span>
-      </div>}
-      { nType.notificationType==='tweet'&&
-        <div>
-         <div>
-            <Avatar src={notify.notificationHeader.images} />
-          
-        </div>
-        <span>
-          <b>{notify.notificationHeader.text}</b> 
-        </span>
-        <span>{notify.notificationContent.text}</span>
-      </div>}
+          {nType.notificationType === "favourite" && (
+            <div>
+              <div>
+                <Avatar src={notify.notificationHeader.images} />
+              </div>
+              <span>
+                <b>{notify.notificationHeader.text}</b>
+              </span>
+              <span>{notify.notificationContent.text}</span>
+            </div>
+          )}
+          {nType.notificationType === "tweet" && (
+            <div>
+              <div>
+                <Avatar src={notify.notificationHeader.images} />
+              </div>
+              <span>
+                <b>{notify.notificationHeader.text}</b>
+              </span>
+              <span>{notify.notificationContent.text}</span>
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <div>
+            <span>
+              <b>{notify.notificationHeader.text}</b>
+            </span>
+          </div>
+        </>
+      )}
     </div>
   );
 };

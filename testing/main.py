5fe0fac8142e502afbc1630f6996d317962fb259
pierreamir123 <pierre.farag00@eng-st.cu.edu.eg>
2022-05-25 01:51:58 +0200

    integrate testing

--- testing/main.py
+++ testing/main.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
+from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 import time
 
